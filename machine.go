6a2d02179a263b4139c4f05021a4604549f85fa6
Chris Doherty <chris.doherty@alteryx.com>
2020-09-28 19:14:00 -0600

    Rework implementation

--- machine.go
+++ machine.go
@@ -12,90 +12,61 @@ type Event string
 // State defines a state that a `Machine` can be in.
 type State string
 
+type Transitions map[State]Events
+
+type Events map[Event]Transition
+
+type Transition struct {
+	state    State
+	decision Decision
+}
+
+type Decision interface{}
+
+// TransitionD registers a deterministic transition from `currentState` to
+// `newState` when `event` occurs.
+func NewTransitionD(state State) Transition {
+	return Transition{state: state}
+}
+
+func NewTransition(decision Decision) Transition {
+	return Transition{decision: decision}
+}
+
 // Machine is a non-deterministic finite automaton.
 type Machine struct {
-	// A map of states to events they can transition away from.
-	transitions  map[State]events
-
 	// The current state of the machine.
 	currentState State
+
+	// A map of states to events they can transition away from.
+	transitions Transitions
 }
 
 // NewMachine creates a new FSM with the `startingState`. If no transitions are
 // registerred against the `startingState`, the FSM will never leave the
 // `startingState`.
-func NewMachine(startingState State) Machine {
+func NewMachine(startingState State, transitions Transitions) Machine {
 	return Machine{
-		transitions:  make(map[State]events),
 		currentState: startingState,
+		transitions:  transitions,
 	}
-}
-
-// TransitionD registers a deterministic transition from `currentState` to
-// `newState` when `event` occurs.
-func (m *Machine) TransitionD(currentState State, event Event, newState State) error {
-	if currentState == "" {
-		return errors.New("Invalid current state")
-	}
-
-	if newState == "" {
-		return errors.New("Invalid new state")
-	}
-
-	return m.addTransition(
-		currentState,
-		event,
-		transition{
-			state: newState,
-		},
-	)
-}
-
-// Transition registers a non-deterministic transition from `currentState`
-// when `event` occurs.
-func (m *Machine) Transition(currentState State, event Event, decision Decision) error {
-	if currentState == "" {
-		return errors.New("Invalid current state")
-	}
-
-	return m.addTransition(
-		currentState,
-		event,
-		transition{
-			decision: decision,
-		},
-	)
-}
-
-func (m *Machine) addTransition(state State, event Event, transition transition) error {
-	if m.events(state) != nil {
-		return fmt.Errorf(
-			"Transition already registered for %v",
-			string(state),
-		)
-	}
-
-	m.transitions[state] = make(events)
-	m.transitions[state][event] = transition
-
-	return nil
 }
 
 // Event triggers a transition. Transitions are only triggered if the current
 // state has the `even` registered as a path to a new state. If the `event` is
 // registered, the FSM will transition to the new state.
-func (m *Machine) Event(event Event, params ...interface{}) error {
+func (m *Machine) Transition(event Event, params ...interface{}) error {
 	// Retrieve the events
 	// If there are no events we've reached an end state so return an error.
 	events := m.events(m.currentState)
 	if events == nil {
-		return fmt.Errorf("End state reached (%v)", m.currentState)
+		return fmt.Errorf("End state (%v)", m.currentState)
 	}
 
 	// If there are events check to see if this state can handle that event
 	// and if not return an error
 	if _, ok := events[event]; !ok {
-		return errors.New("Nothing to do")
+		return errors.New("Invalid transition")
 	}
 
 	// If the transition is a basic transition, do it.
@@ -127,37 +98,12 @@ func (m *Machine) State() State {
 	return m.currentState
 }
 
-func (m *Machine) events(state State) events {
+func (m *Machine) events(state State) Events {
 	if e, ok := m.transitions[state]; ok {
 		return e
 	}
 
 	return nil
-}
-
-// transition represents the method of transition. Either the transition is
-// deterministic and `state` is set, or the transition is non-determinisitic
-// and `decision` is set.
-type transition struct {
-	state    State
-	decision Decision
-}
-
-// events wraps up the Event -> transition mapping for a given state.
-type events map[Event]transition
-
-// Decision represents the decision to be made for a non-deterministic
-// transition.
-type Decision interface{}
-
-// NewDecision performs type checking on the decision function and returns the
-// Decision type.
-func NewDecision(f interface{}) (Decision, error) {
-	if reflect.TypeOf(f).Kind() != reflect.Func {
-		return nil, errors.New("f must be of type reflect.Func")
-	}
-
-	return Decision(f), nil
 }
 
 // executeDecision executes a decision using the supplied `params`. If the
@@ -168,7 +114,10 @@ func executeDecision(d Decision, params ...interface{}) (State, error) {
 	decisionType := decisionValue.Type()
 
 	if decisionType.NumIn() != len(params) {
-		return "", errors.New("Invalid number of params")
+		return "", fmt.Errorf(
+			"Invalid number of params, expected %v",
+			decisionType.NumIn(),
+		)
 	}
 
 	// Iterate over the decision parameters and ensure the kind is the same as
--- machine_blackbox_test.go
+++ machine_blackbox_test.go
@@ -3,55 +3,166 @@ package nfa_test
 import (
 	"testing"
 
-	. "git.alteryx.com/eaas/engine-service/nfa"
-
+	. "github.com/chrisdoherty4/nfa"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMachine(t *testing.T) {
+
+	const (
+		Pending  State = "Pending"
+		Running  State = "Running"
+		Complete State = "Complete"
+		Error    State = "Error"
+	)
+
+	const (
+		Start  Event = "Start"
+		Finish Event = "Finish"
+	)
+
 	t.Run("NewMachine", func(t *testing.T) {
-		_ = NewMachine(State("StartingSate"))
-	})
+		machine := NewMachine(
+			Pending,
+			Transitions{
+				Pending: Events{
+					Start: NewTransitionD(Running),
+				},
+			},
+		)
 
-	t.Run("MachineStartingState", func(t *testing.T) {
-		state := State("Starting")
-		machine := NewMachine(state)
-
-		assert.Equal(t, state, machine.State())
+		assert.Equal(t, Pending, machine.State())
 	})
 
 	t.Run("DeterministicTransition", func(t *testing.T) {
-		startState := State("Starting")
-		finalState := State("Final")
-		event := Event("finalize")
+		machine := NewMachine(
+			Pending,
+			Transitions{
+				Pending: Events{
+					Start: NewTransitionD(Running),
+				},
+			},
+		)
 
-		machine := NewMachine(startState)
-
-		err := machine.TransitionD(startState, event, finalState)
+		err := machine.Transition(Start)
 		assert.Nil(t, err, err)
 
-		err = machine.Event(event)
-		assert.Nil(t, err, err)
-
-		assert.Equal(t, finalState, machine.State())
+		assert.Equal(t, Running, machine.State())
 	})
 
-	t.Run("NonDeterministicTransition", func(t *testing.T) {
-		startState := State("Starting")
-		finalState := State("Final")
-		event := Event("finalize")
+	t.Run("NondeterministicTransition", func(t *testing.T) {
+		machine := NewMachine(
+			Running,
+			Transitions{
+				Running: Events{
+					Finish: NewTransition(func() State {
+						return Complete
+					}),
+				},
+			},
+		)
 
-		machine := NewMachine(startState)
+		err := machine.Transition(Finish)
+		require.Nil(t, err, err)
 
-		err := machine.Transition(startState, event, func() State {
-			return finalState
-		})
-
-		assert.Nil(t, err, err)
-
-		err = machine.Event(event)
-		assert.Nil(t, err, err)
-
-		assert.Equal(t, finalState, machine.State())
+		assert.Equal(t, Complete, machine.State())
 	})
+
+	t.Run("MultipleTransitions", func(t *testing.T) {
+		machine := NewMachine(
+			Pending,
+			Transitions{
+				Pending: Events{
+					Start: NewTransitionD(Running),
+				},
+
+				Running: Events{
+					Finish: NewTransitionD(Complete),
+				},
+			},
+		)
+
+		machine.Transition(Start)
+		assert.Equal(t, Running, machine.State())
+
+		machine.Transition(Finish)
+		assert.Equal(t, Complete, machine.State())
+	})
+
+	t.Run("DecisionParams", func(t *testing.T) {
+		machine := NewMachine(
+			Running,
+			Transitions{
+				Running: Events{
+					Finish: NewTransition(func(state State) State {
+						return state
+					}),
+				},
+			},
+		)
+
+		err := machine.Transition(Finish, Complete)
+		assert.Nil(t, err, err)
+
+		assert.Equal(t, Complete, machine.State())
+	})
+
+	t.Run("IncorrectDecisionParams", func(t *testing.T) {
+		machine := NewMachine(
+			Running,
+			Transitions{
+				Running: Events{
+					Finish: NewTransition(func(i int) State {
+						return Complete
+					}),
+				},
+			},
+		)
+
+		err := machine.Transition(Finish)
+		assert.NotNil(t, err, err)
+	})
+
+	// t.Run("MachineStartingState", func(t *testing.T) {
+	// 	state := State("Starting")
+	// 	machine := NewMachine(state)
+
+	// 	assert.Equal(t, state, machine.State())
+	// })
+
+	// t.Run("DeterministicTransition", func(t *testing.T) {
+	// 	startState := State("Starting")
+	// 	finalState := State("Final")
+	// 	event := Event("finalize")
+
+	// 	machine := NewMachine(startState)
+
+	// 	err := machine.TransitionD(startState, event, finalState)
+	// 	assert.Nil(t, err, err)
+
+	// 	err = machine.Event(event)
+	// 	assert.Nil(t, err, err)
+
+	// 	assert.Equal(t, finalState, machine.State())
+	// })
+
+	// t.Run("NonDeterministicTransition", func(t *testing.T) {
+	// 	startState := State("Starting")
+	// 	finalState := State("Final")
+	// 	event := Event("finalize")
+
+	// 	machine := NewMachine(startState)
+
+	// 	err := machine.Transition(startState, event, func() State {
+	// 		return finalState
+	// 	})
+
+	// 	assert.Nil(t, err, err)
+
+	// 	err = machine.Event(event)
+	// 	assert.Nil(t, err, err)
+
+	// 	assert.Equal(t, finalState, machine.State())
+	// })
 }
